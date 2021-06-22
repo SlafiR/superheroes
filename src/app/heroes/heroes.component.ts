8f2b8e75001737428d905336c16d6c55bdf0216c
ncio123 <juan175691996@gmail.com>
2021-06-21 23:07:57 -0500

    Se hacen ajustes al codigo

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 
+const URL_HEROES = "https://akabab.github.io/superhero-api/api"
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -8,17 +10,16 @@ import { HttpClient } from '@angular/common/http'
 })
 export class HeroesComponent implements OnInit {
 
-  public heroes:any
-  constructor(private http:HttpClient) {
+  public heroes: any[] = []
 
-   }
+  constructor(private http: HttpClient) { }
 
-  ngOnInit(): void { 
-    this.http.get("https://akabab.github.io/superhero-api/api/all.json")
-    .subscribe( _heroes => {
-      console.log(_heroes)
-      this.heroes = _heroes
-    })
+  ngOnInit(): void {
+    this.http.get(`${URL_HEROES}/all.json`)
+      .subscribe((_heroes: any) => {
+        console.log(_heroes)
+        this.heroes = _heroes
+      })
   }
 
 }
